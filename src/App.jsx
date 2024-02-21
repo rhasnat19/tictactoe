453a8b5c43c521c0ebdac57b24cbcfe29491889f
rhasnat191 <r.hasnat19@gmail.com>
2024-02-21 17:22:42 +0500

    basic state lifting up between two child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
+
 function App() {
-  return <h1>React Tic-Tac-Toe</h1>;
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currentlyActivePlayer) =>
+      currentlyActivePlayer === "X" ? "0" : "X"
+    );
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="0"
+            isActive={activePlayer === "0"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
+      </div>
+    </main>
+  );
 }
 
 export default App;
